b1ec8c383d43cc74382d0aa3f3bec3f3cab85387
Alexandre Louisnard <alouisnard@portalp.com>
2022-05-24 10:59:39 +0200

    Updated TypeScript.

--- typescript/utils.ts
+++ typescript/utils.ts
@@ -2,9 +2,38 @@
 
 /* #region Javascript */
 /**
- * Sleeps for the given amount of time, in milliseconds.
+ * Returns a deep copy/clone of @param src .
  *
- * To be used in an async function such as:
+ * @param src the object to deep copy
+ * @returns a deep copy of @param src
+ */
+export function deepCopy<T>(src: T): T {
+  let target: T = (Array.isArray(src) ? [] : {}) as T;
+  for (let prop in src) {
+    let value = src[prop];
+    if (value && typeof value === 'object') {
+      target[prop] = deepCopy(value);
+    } else {
+      target[prop] = value;
+    }
+  }
+  return target;
+}
+
+/**
+ * Returns a deep copy/clone of @param src .
+ *
+ * NOTE: works only for serializable objects. Non-serializable properties will be skipped.
+ *
+ * @param src the serializable object to deep copy
+ * @returns a deep copy of @param src
+ */
+export function deepCopyForSerializableObjects<T>(src: T): T {
+  return JSON.parse(JSON.stringify(src));
+}
+
+/**
+ * Sleeps for the given amount of time, in milliseconds. To be used in an async function such as:
  *
  * async function demo() {
  *  console.log('Start');
@@ -12,18 +41,18 @@
  *  console.log('Two seconds later');
  * }
  *
- * @param {number} ms
+ * @param ms
  * @returns
  */
 export function sleep(ms: number) {
-  return new Promise<void>((resolve) => setTimeout(resolve, ms));
+  return new Promise<void>((resolve) => global.setTimeout(resolve, ms));
 }
 
 /**
  * Returns a unique hash code for the given string.
  *
- * @param {string} str the string to hash
- * @returns {number} the hash code
+ * @param str the string to hash
+ * @returns the hash code as a 32 bit integer
  */
 export function hashCode(str: string): number {
   let hash = 0;
@@ -34,12 +63,13 @@ export function hashCode(str: string): number {
   for (i = 0; i < str.length; i++) {
     chr = str.charCodeAt(i);
     hash = (hash << 5) - hash + chr;
-    hash |= 0; // Convert to 32bit integer
+    hash |= 0; // Convert to 32 bit integer
   }
   return hash;
 }
 
 export type EqualityCheck = 'X_EQUALS_Y' | 'X_SUBSET_OF_Y';
+
 /**
  * Depending on {equalityCheck}, indicates whether:
  * * 'X_EQUALS_Y' (default): {x} and {y} are equal or have the same properties and the same properties values.
@@ -79,14 +109,16 @@ export function equals(x: any, y: any, equalityCheck: EqualityCheck = 'X_EQUALS_
         if (x.length !== y.length) {
           return false;
         }
+
         break;
       case 'X_SUBSET_OF_Y':
         if (x.length > y.length) {
           return false;
         }
+
         break;
       default:
-        console.log(`Utils: equals(): ERROR: unknown equality check: ${equalityCheck}`);
+        console.log(`utils: equals(): ERROR: unknown equality check: ${equalityCheck}`);
         return false;
     }
   }
@@ -112,40 +144,30 @@ export function equals(x: any, y: any, equalityCheck: EqualityCheck = 'X_EQUALS_
     case 'X_SUBSET_OF_Y':
       return xKeys.every((xKey) => equals(x[xKey], y[xKey], equalityCheck));
     default:
-      console.log(`equals(): ERROR: unknown equality check: ${equalityCheck}`);
+      log(`equals(): ERROR: unknown equality check: ${equalityCheck}`);
       return false;
   }
 }
 
 /**
  * Returns the value of a nested property or a default value if some of the property chain is undefined.
+ *
  * Example: get(test, ['nested', 'property', 2, 'prop'], 'default value') will return test.nested.property[2].prop if all the chain is defined, or 'default value'.
+ *
+ * NOTE: with TypeScript, this method is useless as we can use optional chaining instead: test?.nested?.property?.[2]?.prop ?? 'default value'
  */
-// $FlowFixMe
-export const get: (object: any, pathToNestedProperty: (string | number)[], defaultValue?: any) => any = (object, pathToNestedProperty, defaultValue = undefined) =>
+export const get: <T = any>(object: any, pathToNestedProperty: (string | number)[], defaultValue?: T) => T = (object, pathToNestedProperty, defaultValue = undefined) =>
   pathToNestedProperty.reduce((xs, x) => (xs && xs[x] !== undefined ? xs[x] : defaultValue), object);
 
 /**
- * Returns a new object instance with updated properties.
- *
- * @export
- * @param {{}} oldObject
- * @param {{ [key: string]: any }} newValues
- * @returns
- */
-export function updateObject<X>(oldObject: X, newValues: { [key: string]: any }): X {
-  // Encapsulate the idea of passing a new object as the first parameter
-  // to Object.assign to ensure we correctly copy data instead of mutating
-  return Object.assign({}, oldObject, newValues);
-}
-
-/**
- * Flow type definition for an object with an {id} attribute.
+ * TS type definition for an object with an {id} attribute.
  */
 export type IDable = { id: any };
 
 /**
  * Returns a new array in which any item such as {item.id === itemId} has been updated by the updateItemCallback(item) function.
+ *
+ * This function is mutating/non-mutating depending on @param updateItemCallback() being mutating/non-mutating.
  *
  * @export
  * @param {Array} array
@@ -197,23 +219,11 @@ export function addOrReplaceItemInArray<X>(array: (X & IDable)[], newItem: X & I
 }
 
 /**
- * RN Bug fix: replacement of the spread operator on Buffer's (...) because it is not working fine on Android in release mode.
- * https://github.com/facebook/react-native/issues/21734
+ * Indicates whether the given @param object is a @type Promise.
  *
  * @export
- * @param {Buffer} buffer
+ * @param object
  * @returns
- */
-export function spread(buffer: Buffer) {
-  return Array.prototype.slice.call(buffer, 0);
-}
-
-/**
- * Indicates whether the given {object} is a {Promise}.
- *
- * @export
- * @param {*} object
- * @returns {boolean}
  */
 export function isPromise(object: any): boolean {
   if (Promise && Promise.resolve) {
@@ -221,6 +231,7 @@ export function isPromise(object: any): boolean {
   }
   throw new Error('Promise not supported in your environment');
 }
+
 /* #endregion */
 
 /* #region Strings format */
@@ -228,20 +239,20 @@ export function isPromise(object: any): boolean {
  * Returns the formatted MAC address with the given {divisionChar}. For example if divisionChar = ":", MAC = "01:AA:22:33:BB:44".
  * @param rawMac a raw MAC address (12 alphanum chars) such as 01AA2233BB44
  * @param divisionChar a division character such as ':', '.', '-', etc
- * @return the formatted MAC address {string} or <b>null</b>
+ * @return the formatted MAC address {string} or an empty {string}
  */
-export function formatMacAddress(rawMac: string, divisionChar: string): ?string {
-  if (rawMac.length !== 12) {
-    return null;
+export function formatMacAddress(rawMac: string, divisionChar: string): string {
+  let macStr = '';
+  if (rawMac.length === 12) {
+    macStr = rawMac
+      .replace(/(.{2})/g, `$1${divisionChar}`)
+      .substring(0, 17)
+      .toLocaleUpperCase();
+    if (macStr.length !== 17) {
+      macStr = '';
+    }
   }
-  const mac: string = rawMac
-    .replace(/(.{2})/g, `$1${divisionChar}`)
-    .substring(0, 17)
-    .toLocaleUpperCase();
-  if (mac.length !== 17) {
-    return null;
-  }
-  return mac;
+  return macStr;
 }
 
 /**
@@ -264,7 +275,12 @@ export function normalizeMacAddress(formattedMac: string): string {
  * @return
  */
 export function checkMacAddressValidity(mac: string): boolean {
-  if (mac.length !== 12 /* raw MAC */ && mac.length !== 17 /* MAC with separator */) {
+  if (
+    mac.length !== 12 &&
+    /* raw MAC */
+    mac.length !== 17
+    /* MAC with separator */
+  ) {
     return false;
   }
   return /^([a-fA-F0-9]{2}[.:-]?){5}[a-fA-F0-9]{2}$/.test(mac);
@@ -277,7 +293,7 @@ export function checkMacAddressValidity(mac: string): boolean {
  * @param {?Date} date
  * @returns {string}
  */
-export function dateToYYYYMMDDHHMMSS(date: ?Date): string {
+export function dateToYYYYMMDDHHMMSS(date: Date | null | undefined): string {
   if (!date) {
     return '';
   }
@@ -293,7 +309,7 @@ export function dateToYYYYMMDDHHMMSS(date: ?Date): string {
  * @param {?string} phone
  * @returns {boolean}
  */
-export function isPhoneNumber(phone: ?string): boolean {
+export function isPhoneNumber(phone: string | null | undefined): boolean {
   if (!phone) {
     return false;
   }
@@ -317,10 +333,10 @@ export function removeForbiddenJsonKeyCharacters(key: string): string {
  * For the given {@code number}, sets the bit at {@code bitIndex} to {@code bitValue}, and returns the new number value.
  *
  * @export
- * @param {number} number
- * @param {number} bitIndex
- * @param {number} bitValue
- * @returns {number}
+ * @param number
+ * @param bitIndex
+ * @param bitValue
+ * @returns
  */
 export function setBit(number: number, bitIndex: number, bitValue: number): number {
   switch (bitValue) {
